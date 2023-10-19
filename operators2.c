79ee29ba7b2789140b1c54fb4242ebdcca5e0a98
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-19 13:05:00 -0700

    fixed swap

--- operators2.c
+++ operators2.c
@@ -45,6 +45,11 @@ void swap(stack_t **stack, unsigned int line_n)
 {
 	int temp;
 
+	if ((*stack) == NULL)
+        {
+                fprintf(stderr, "L%d: can't swap, stack too short\n", line_n);
+                exit(EXIT_FAILURE);
+        }
 	if ((*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_n);
