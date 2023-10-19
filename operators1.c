f986eb4e713366d25d957f7d4ca7bfc3650b601f
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-19 08:45:24 -0700

    added pall funciton

--- operators1.c
+++ operators1.c
@@ -28,6 +28,12 @@ void push(stack_t **stack, unsigned int line_n)
 }
 void pall(stack_t **stack, unsigned int line_n)
 {
+	stack_t *temp;
 
+	temp = *stack;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
 }
-
